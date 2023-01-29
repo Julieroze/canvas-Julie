24dce752e4d3978252a8ce0ac5ea276981345b12
unknown <jw.yihmm@gmail.com>
2023-01-29 14:38:55 -0800

    week 3 project

---
+++ app.js
@@ -0,0 +1,41 @@
+const lineWidth = document.getElementById("line-width")
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
+
+
+function onMove(event){
+    if(isPainting) {
+    
+     ctx.lineTo(event.offsetX, event.offsetY);
+     ctx.stroke();
+     return
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+
+function startPainting(){
+  isPainting = true;
+  ctx.beginPath();
+}
+
+function CancelPainting(){
+  isPainting = false;
+  
+}
+
+function onLineWidthChange(event) {
+    console.log(event);
+    ctx.lineWidth=event.target.value
+}
+
+canvas.addEventListener("mousemove", onMove)
+canvas.addEventListener("mousedown", startPainting)
+canvas.addEventListener("mouseup", CancelPainting)
+canvas.addEventListener("mouseleave", CancelPainting)
+lineWidth.addEventListener("change", onLineWidthChange)
+
